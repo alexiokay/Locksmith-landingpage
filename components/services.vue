6f9c929352e36f860075089c02b8d1b418d9cfa6
alexispace <papadi166@gmail.com>
2023-05-28 22:03:06 +0200

    cosmetical changes

--- components/services.vue
+++ components/services.vue
@@ -5,21 +5,22 @@ div(class="w-full md:px-12 flex flex-col text-center space-y-12")
         p(class="text-2xl font-semibold text-[#7c8e98]") Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.
     div(class="flex flex-col lg:flex-row w-full lg:justify-around h-auto items-center justify-center gap-y-12")
         
-        div(class="flex flex-col lg:w-1/5 w-3/4 space-y-6 text-[#02374b] lg:text-start items-center lg:items-start")
-            nuxt-img(src="icons/car_services.png" width="100" h="30")
+        div(class="flex flex-col lg:w-1/5 w-3/4 gap-y-6 text-[#02374b] lg:text-start items-center lg:items-start")
+            nuxt-img(src="icons/car_services.png" width="150" h="45")
             h3(class="font-bold text-4xl") Automotive Services
-            p(class="text-xl") Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptatibus repudiandae dignissimos, omnis distinctio deserunt eum. Accusantium ad neque quos necessitatibus beatae cupiditate, tenetur praesentium provident. Eaque maxime consectetur ad.
-            buttonsLearnMore(text="Learn More" class="h-[5rem] w-[17rem] text-xl")
-        div(class="flex flex-col lg:w-1/5 w-3/4 space-y-6 text-[#02374b]  lg:text-start items-center lg:items-start")
-            nuxt-img(src="icons/commercial_services.png" width="100" h="30")
-            h3(class="font-bold text-4xl") Commercial Services
-            p(class="text-xl") Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptatibus repudiandae dignissimos, omnis distinctio deserunt eum. Accusantium ad neque quos necessitatibus beatae cupiditate, tenetur praesentium provident. Eaque maxime consectetur ad.
-            buttonsLearnMore(text="Learn More" class="h-[5rem] w-[17rem] text-xl")
-        div(class="flex flex-col lg:w-1/5 w-3/4 space-y-6 text-[#02374b]  lg:text-start items-center lg:items-start")
-            nuxt-img(src="icons/residental_services.png" width="100" h="30")
+            p(class="text-2xl text-[#7c8e98]") Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptatibus repudiandae dignissimos, omnis distinctio deserunt eum. Accusantium ad neque quos necessitatibus beatae cupiditate, tenetur praesentium provident. Eaque maxime consectetur ad.
+            buttonsLearnMore(text="Learn More" class="h-[5rem] w-[17rem] text-xl mt-6")
+        div(class="flex flex-col lg:w-1/5 w-3/4 gap-y-6 text-[#02374b]  lg:text-start items-center lg:items-start")
+            nuxt-img(src="icons/residental_services.png" width="150" h="45")
             h3(class="font-bold text-4xl") Residential Services
-            p(class="text-xl") Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptatibus repudiandae dignissimos, omnis distinctio deserunt eum. Accusantium ad neque quos necessitatibus beatae cupiditate, tenetur praesentium provident. Eaque maxime consectetur ad.
-            buttonsLearnMore(text="Learn More" class="h-[5rem] w-[17rem] text-xl")
+            p(class="text-2xl text-[#7c8e98]") Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptatibus repudiandae dignissimos, omnis distinctio deserunt eum. Accusantium ad neque quos necessitatibus beatae cupiditate, tenetur praesentium provident. Eaque maxime consectetur ad.
+            buttonsLearnMore(text="Learn More" class="h-[5rem] w-[17rem] text-xl mt-6")
+        div(class="flex flex-col lg:w-1/5 w-3/4 gap-y-6 text-[#02374b]  lg:text-start items-center lg:items-start")
+            nuxt-img(src="icons/commercial_services.png" width="150" h="45")
+            h3(class="font-bold text-4xl") Commercial Services
+            p(class="text-2xl text-[#7c8e98]") Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quo voluptatibus repudiandae dignissimos, omnis distinctio deserunt eum. Accusantium ad neque quos necessitatibus beatae cupiditate, tenetur praesentium provident. Eaque maxime consectetur ad.
+            buttonsLearnMore(text="Learn More" class="h-[5rem] w-[17rem] text-xl mt-6")
+        
 
 </template>
 
